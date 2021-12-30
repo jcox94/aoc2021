38f6ee8829d2b4df27ab3060b580ec826ba86822
Jacob Cox <mocio333@gmail.com>
2021-12-30 18:23:03 -0500

    partial day 4 cleanup

--- Sources/aoc2021/aoc.swift
+++ Sources/aoc2021/aoc.swift
@@ -4,7 +4,7 @@ import Foundation
 struct AOC {
     static func main() throws {
         let contents = try String(contentsOfFile: "files/day4.txt", encoding: .utf8)
-        let solution = try d4p2(contents)
+        let solution = try d4p1(contents)
         print(solution)
     }
 }
--- Sources/aoc2021/day4.swift
+++ Sources/aoc2021/day4.swift
@@ -16,105 +16,77 @@ struct Board {
             }
             return true
         }
-        outer2: for i in 0..<5 {
+        outer: for i in 0..<5 {
             for j in 0..<5 {
                 if !numbers.contains(rows[i + (j * 5)]) {
-                    continue outer2
+                    continue outer
                 }
             }
             return true
         }
         return false
     }
+
+    mutating func append(_ num: Int) {
+        rows.append(num)
+    }
 }
 
-class Parser {
-    var content: String
+struct Parser {
+    var content: Substring
 
-    enum ParseError: Error {
-        case InvalidCharacter
-    }
-
-    init(_ content: String) {
-        self.content = content
-    }
-
-    //@TODO: make this not horrible
-    func run() throws -> ([Int], [Board]) {
-        var numbers: [Int] = []
-        var boards: [Board] = []
-        var parsingBoards = false
-        var inNumber = false
-        var currentNumber: Int = 0
-        var currentBoard = Board()
-        var extraNewLine = false
-        //parse numbers
-        for char in content {
-            if !parsingBoards {
-                switch char {
-                case "\n":
-                    numbers.append(currentNumber)
-                    currentNumber = 0
-                    parsingBoards = true
-                case ",":
-                    numbers.append(currentNumber)
-                    currentNumber = 0
-                default:
-                    guard char.isWholeNumber else {
-                        print("Got char of \(char)")
-                        throw ParseError.InvalidCharacter
-                    }
-                    currentNumber *= 10
-                    currentNumber += char.wholeNumberValue!
-                }
-            } else {
-                switch char {
-                case "\n":
-                    if !extraNewLine {
-                        extraNewLine = true
-                        continue
-                    }
-                    if !inNumber && currentBoard.rows.count > 0 {
-                        boards.append(currentBoard)
-                        currentBoard = Board()
-                    } else {
-                        currentBoard.rows.append(currentNumber)
-                        currentNumber = 0
-                        inNumber = false
-                    }  
-                case " ":
-                    if inNumber {
-                        currentBoard.rows.append(currentNumber)
-                        currentNumber = 0
-                        inNumber = false
-                    } else {
-                        continue
-                    }
-                default:
-                    guard char.isWholeNumber else {
-                        print("Got char of \(char)")
-                        throw ParseError.InvalidCharacter
-                    }
-                    inNumber = true
-                    currentNumber *= 10
-                    currentNumber += char.wholeNumberValue!
-                }
+    mutating func run() throws -> ([Int], [Board]) {
+        var numbers = [Int]()
+        var boards = [Board]()
+        var currentNumber = ""
+        // Parse numbers
+        outer: while let char = content.popFirst() {
+            switch char {
+            case ",":
+                numbers.append(Int(currentNumber)!)
+                currentNumber = ""
+            case "\n":
+                numbers.append(Int(currentNumber)!)
+                break outer
+            default:
+                currentNumber.append(char)
             }
         }
-        boards.append(currentBoard)
+        content.trimPrefix(while: \.isNewline)
+        var currentBoard = Board()
+        // Parse boards
+        while let char = content.popFirst() {
+            switch char {
+            case " ":
+                currentBoard.append(Int(currentNumber)!)
+                currentNumber = ""
+                content.trimPrefix(while: \.isWhitespace)
+            case "\n":
+                currentBoard.append(Int(currentNumber)!)
+                currentNumber = ""
+                if content.first == "\n" {
+                    boards.append(currentBoard)
+                    currentBoard = Board()
+                    let _ = content.popFirst()
+                }
+                content.trimPrefix(while: \.isWhitespace)
+            default:
+                currentNumber.append(char)
+            }
+        }
         return (numbers, boards)
     }
 }
 
+// @TODO: Cleanup both parts below
+
 func d4p1(_ contents: String) throws -> Int {
-    let parser = Parser(contents)
+    var parser = Parser(content: contents[...])
     let (numbers, boards) = try parser.run()
     for i in 4..<numbers.count {
         let range = numbers[0...i]
         for board in boards {
             if board.checkWin(numbers: range) {
-                print(range)
-                print(board)
                 var unchecked = 0
                 for num in board.rows {
                     if !range.contains(num) {
@@ -129,7 +101,7 @@ func d4p1(_ contents: String) throws -> Int {
 }
 
 func d4p2(_ contents: String) throws -> Int {
-    let parser = Parser(contents)
+    var parser = Parser(content: contents[...])
     let (numbers, boards) = try parser.run()
     var winningBoards: Set<Int> = []
     for i in 4..<numbers.count {
